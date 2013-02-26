364e867707d9c463b84fecd6a70d3b1a7a8ba761
cedeel <chris@cedeel.com>
2013-02-26 17:14:17 +0100

    PEP 8 compliance

--- aofetch.py
+++ aofetch.py
@@ -4,19 +4,20 @@ import base64
 import suds
 from xml.dom import minidom
 
+
 def get_image(session, base, path):
     url = "http://ao.sa.dk/LAView/ImageServer/Service1.asmx?WSDL"
     client = suds.client.Client(url)
     response = client.service.getImage2(session, base + path)
-    print ("Error Code: " + response.errorCode +
-           "Network Load: " + str(response.networkLoad))
+    print("Error Code: " + response.errorCode +
+          "Network Load: " + str(response.networkLoad))
     if response.errorCode == "Ok":
         image64 = response.imageData
         image = base64.standard_b64decode(image64)
-        filename = 'img/' + ( base + path ).replace("/", ".")
+        filename = 'img/' + (base + path).replace("/", ".")
         file = open(filename, "wb")
         file.write(image)
-        print ("Writing to file " + filename)
+        print("Writing to file " + filename)
         file.close()
 
 xmldoc = minidom.parse(sys.argv[1])
